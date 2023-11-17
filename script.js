03de95f20de0ac624871ee3c57199686ee16c1f2
Rajan Kumar <106719815+iamrajankumar@users.noreply.github.com>
2023-11-17 22:40:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+// If Else Condition in JavaScript
+
+
+// let input;
+
+// input = '';
+
+// if(input===1){
+//         document.write("continu......");
+// }else if(input==="y"){
+//         document.write("continu......");
+// }else if(input==="yes"){
+//         document.write("continu......");
+// }else if(input===0){
+//         document.write("end......");
+// }else if(input==="n"){
+//         document.write("end......");
+// }else if(input==="no"){
+//         document.write("end......");
+// }else{
+//         document.write("worng input......");
+// }
+
+// switch case comprison
+// let input;
+// input = 'yes';
+
+// switch(input){
+//         case 1:
+//                 document.write("continue......");
+//                 break;
+//         case "y":
+//                 document.write("continue.....");
+//                 break;
+//         case "yes":
+//                 document.write("continue......");
+//                 break;
+//         case 0:
+//                 document.write("end..........");
+//                 break;
+//         case "n":
+//                 document.write("end..........");
+//                 break;
+//         case "no":
+//                 document.write("end..........");
+//                 break;
+//         default :
+//                 document.write("wrong input....")
+// }
+
+// output = continue....
+
